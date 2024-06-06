087fa8cce6677ac4c179fe7515ff33aaef1308ba
Jason Yu <jasontech1998@gmail.com>
2024-06-06 16:09:20 -0700

    setup project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1 className="text-xl">Testing H1</h1>
+        <Button>Click me</Button>
+      </div>
+    </main>
+  );
+}

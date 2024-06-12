843957be884ca034312820ad8080b2dfd0f59241
Jason Yu <jasontech1998@gmail.com>
2024-06-11 20:07:32 -0700

    quick update to main page

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,9 @@ export default function Home() {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Spotify Shared
       </h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        Quickly share your saved songs from any album
+      </p>
       <Login />
     </main>
   );

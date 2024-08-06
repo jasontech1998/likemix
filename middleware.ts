df5ae1339e7174e8f217fcb76013d5d476fe6e9d
Jason Yu <jasontech1998@gmail.com>
2024-08-06 15:52:14 -0700

    fix middleware for auth

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,35 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { getToken } from "next-auth/jwt";
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get('next-auth.session-token') || req.cookies.get('__Secure-next-auth.session-token');
-  if (!token) {
-    return NextResponse.redirect(new URL('/', req.url));
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  // If the user is authenticated and trying to access the root path, redirect to /home
+  if (token && request.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  // If the user is not authenticated and trying to access a protected route, redirect to the root
+  if (!token && request.nextUrl.pathname !== "/") {
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   return NextResponse.next();
 }
+
 export const config = {
-  matcher: ['/album/:path*', '/dashboard'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
 };
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -5,6 +5,11 @@ declare module 'next-auth' {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    token?: string
+    token?: string,
+    user?: {
+      email: string;
+      name: string;
+      image: string;
+    }
   }
 }
